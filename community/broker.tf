aba2fe4c52ac405f7f288c1674cc04fdc088ccdf
Ben Lackey <ben.lackey@outlook.com>
2019-03-10 11:52:02 -0700

    mystery char in the output causing beautfiy to choke

--- community/broker.tf
+++ community/broker.tf
@@ -4,7 +4,7 @@ resource "oci_core_instance" "broker" {
   availability_domain = "${lookup(data.oci_identity_availability_domains.availability_domains.availability_domains[0],"name")}"
   shape               = "${var.broker["shape"]}"
   subnet_id           = "${oci_core_subnet.subnet.id}"
-  fault_domain = "${lookup(data.oci_identity_fault_domains.fault_domains.fault_domains[count.index%3],"name")}"
+  fault_domain        = "${lookup(data.oci_identity_fault_domains.fault_domains.fault_domains[count.index%3],"name")}"
 
   source_details {
     source_id   = "${var.images[var.region]}"
@@ -12,8 +12,8 @@ resource "oci_core_instance" "broker" {
   }
 
   create_vnic_details {
-    subnet_id           = "${oci_core_subnet.subnet.id}"
-    hostname_label = "broker-${count.index}"
+    subnet_id        = "${oci_core_subnet.subnet.id}"
+    hostname_label   = "broker-${count.index}"
     assign_public_ip = "false"
   }
 
@@ -23,12 +23,11 @@ resource "oci_core_instance" "broker" {
     user_data = "${base64encode(join("\n", list(
       "#!/usr/bin/env bash",
       file("../scripts/broker.sh")
-    )}"
+    )))}"
   }
 
   count = "${var.broker["node_count"]}"
 }
-
 
 resource "oci_core_volume" "broker" {
   count               = "${var.broker["node_count"] * var.broker["disk_count"]}"
@@ -44,7 +43,8 @@ resource "oci_core_volume_attachment" "broker" {
   compartment_id  = "${var.compartment_ocid}"
   instance_id     = "${oci_core_instance.broker.*.id[count.index % var.broker["node_count"]]}"
   volume_id       = "${oci_core_volume.broker.*.id[count.index]}"
+}
 
-output "Kafka Broker Private IPs" {
-  value = "${join(",", oci_core_instance.broker.*.private_ip)}"
+output "Kafka Broker Public IPs" {
+  value = "${join(",", oci_core_instance.broker.*.public_ip)}"
 }
