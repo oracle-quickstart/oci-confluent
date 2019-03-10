611f8b4f8888d626bc46299fa3d47d8df00078a6
Ben Lackey <ben.lackey@outlook.com>
2019-03-10 14:04:53 -0700

    ready to clean up myid

--- scripts/zookeeper.sh
+++ scripts/zookeeper.sh
@@ -1,9 +1,9 @@
 echo "Running zookeeper.sh"
 
-#######################################################
-################# Configure ZooKeeper #################
-#######################################################
 echo "Configuring ZooKeeper..."
+
+# Here's the install doc:
+# https://docs.confluent.io/current/installation/installing_cp/rhel-centos.html#zk
 
 # 1. Navigate to the ZooKeeper properties file (/etc/kafka/zookeeper.properties) file and modify as shown.
 
@@ -14,9 +14,14 @@ dataDir=/var/lib/zookeeper/
 clientPort=2181
 initLimit=5
 syncLimit=2
+server.0=zookeeper-0:2888:3888
 server.1=zookeeper-0:2888:3888
 server.2=zookeeper-0:2888:3888
-server.3=zookeeper-0:2888:3888
 autopurge.snapRetainCount=3
 autopurge.purgeInterval=24
 " > /etc/kafka/zookeeper.properties
+
+echo "'0'" > /var/lib/zookeeper/myid
+
+#echo "Starting ZooKeeper..."
+#systemctl start confluent-zookeeper
