7836ba6aafe4b575f3518b0c1056cfe209136a85
Collin Poczatek <collin.poczatek@gmail.com>
2019-03-13 16:21:53 -0700

    clean shell, closes #12

--- scripts/zookeeper.sh
+++ scripts/zookeeper.sh
@@ -21,22 +21,13 @@ echo "autopurge.snapRetainCount=3
 autopurge.purgeInterval=24
 " >> /etc/kafka/zookeeper.properties
 
-
-# Update zookeeper.properties with zookeeper node info
-#for index in `seq 0 $((zookeeperNodeCount-1))` ; do echo "server.${index}=zookeeper-${index}:2888:3888" >> /tmp/zk.txt  ; done ;
-
-
 nodeIndex=`hostname | sed 's/zookeeper-//'`
 echo "$nodeIndex" > /var/lib/zookeeper/myid
 
-###### unclear if any of this is needed... try to remove....
+###### package leaves files as root owned, causes issues
 chown cp-kafka /var/lib/zookeeper/myid
 chgrp confluent /var/lib/zookeeper/myid
 chmod 666 /var/lib/zookeeper/myid
-
-###### this came from a suggestion in the logs... check if needed and maybe remove....
-chown cp-kafka:confluent /var/log/confluent
-chmod u+wx,g+wx,o= /var/log/confluent
 
 echo "Starting ZooKeeper..."
 systemctl start confluent-zookeeper
